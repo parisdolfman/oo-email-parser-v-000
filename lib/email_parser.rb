0da123a520935d9feb6dd22c1bd868b084c35fa7
Paris Dolfman <parisdolfman@gmail.com>
2018-10-15 20:11:09 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,11 +7,11 @@ class EmailParser
 attr_accessor :email
 
  def initialize(email) #initializes with argument of a string
-   @email = email #sets variable from argument to eq instance variable available to all methods within EmailParser class 
+   @email = email #sets variable from argument to eq instance variable available to all methods within EmailParser class
  end
 
  def parse  #method
-  email.delete(',').split.uniq #.delete(',')
- end
+  email.delete(',').split.uniq #.delete(',') returns copy of string - email - with characters in intersection of argument deleted = deletes all ','
+end #.split splits each string and returns an array # .uniq returns true if array elements are all different
 
 end
